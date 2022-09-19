a1be3e06c10dfc7c0d3acafe1d26143679a44f87
Updesh Pr. Singh <updesh071@gmail.com>
2022-09-19 18:17:36 +0530

    Add files via upload

---
+++ Script/CubeMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMove : MonoBehaviour
+{
+    public float speed = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(20.0f, 1.0f, 0.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float newX = transform.position.x - speed;
+        if (newX < -1)
+        {
+            newX = 20.0f;
+        }
+        transform.position = new Vector3(newX, 1.0f, 0.0f);
+        
+    }
+}
---
+++ Script/CubePointer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubePointer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Response Pointer Touch the cube
+    public void PointerEnter()
+    {
+        //Change the color of the cube to blue
+        GetComponent<Renderer>().material.color = Color.blue;
+    } 
+    //Response Pointer Exit the cube
+    public void PinterExit()
+    {
+        //change the color of the cube to white
+        GetComponent<Renderer>().material.color = Color.white;
+
+    }
+    //Response to Pointer click Event
+    public void PointerClick()
+    {
+        //kill the enemy
+        Destroy(gameObject);
+    }
+}
